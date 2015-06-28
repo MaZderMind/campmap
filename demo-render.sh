2a4e726db07385e0db4579683f1a7d809191c2c5
MaZderMind <github@mazdermind.de>
2015-06-28 19:21:12 +0200

    demo render script

--- demo-render.sh
+++ demo-render.sh
@@ -1,3 +1,7 @@
 BBOX="13.303317 53.029361 13.313832 53.033845"
 
-nik2img.py --no-open --bbox $BBOX styles/noclinks.xml 0-noclinks.png
+nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/noc.xml noc.png
+nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/tents.xml tents.png
+nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/buildings.xml buildings.png
+#nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/ways.xml ways.png
+#nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/landuse.xml landuse.png
