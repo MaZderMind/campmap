ca0a96894b8aede75bf327506061856c88d7622e
MaZderMind <github@mazdermind.de>
2015-06-29 19:36:32 +0200

    Use render-tool

--- demo-render.sh
+++ demo-render.sh
@@ -1,11 +1,11 @@
-BBOX="13.303317 53.029361 13.313832 53.033845"
+BBOX="13.301053047180174,53.028206650012166,13.316202163696289,53.03405226930914"
 
 rm layer-*.png
-nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/landuse.xml layer-0-landuse.png
-nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/ways.xml layer-1-ways.png
-nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/buildings.xml layer-2-buildings.png
-nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/tents.xml layer-3-tents.png
-nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/noc.xml layer-4-noc.png
-nik2img.py --no-open --dimensions 1920 1080 --bbox $BBOX styles/gpscoords.xml layer-5-gpscoords.png
+render --bbox $BBOX --zoom=18 --scale=2 --style=styles/landuse.xml --file=layer-0-landuse
+render --bbox $BBOX --zoom=18 --scale=2 --style=styles/ways.xml --file=layer-1-ways
+render --bbox $BBOX --zoom=18 --scale=2 --style=styles/buildings.xml --file=layer-2-buildings
+render --bbox $BBOX --zoom=18 --scale=2 --style=styles/tents.xml --file=layer-3-tents
+render --bbox $BBOX --zoom=18 --scale=2 --style=styles/noc.xml --file=layer-4-noc
+render --bbox $BBOX --zoom=18 --scale=2 --style=styles/gpscoords.xml --file=layer-5-gpscoords
 
 gm convert -compose Atop layer-*.png -flatten X-composition.png
