8314c23f72b162a083d57d0d2bba2df2c7cdd5f2
MaZderMind <github@mazdermind.de>
2015-06-29 11:39:00 +0200

    Layer Names

--- update-data.sh
+++ update-data.sh
@@ -5,5 +5,6 @@ BBOX=13.2776,53.0222,13.345,53.0442
 wget "http://maps.c3voc.de/api/0.6/map?bbox=$BBOX" -O camp.osm
 /home/pko/osm2pgsql/osm2pgsql -d campmap --hstore-all -c -S camp.style --slim camp.osm
 
-tirex-batch map=campmap-buildings,campmap-datenklo_area,campmap-noc,campmap-railway,campmap-tents bbox=$BBOX z=16-20
+echo "batch-rerendering tiles"
+tirex-batch map=campmap-landuse,campmap-ways,campmap-buildings,campmap-tents,campmap-noc,campmap-gpscoords,campmap-tents bbox=$BBOX z=16-20
 
