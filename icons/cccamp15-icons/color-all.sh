3fdf33886e31b457eef16f5a5441d940b433919d
MaZderMind <github@mazdermind.de>
2015-07-19 23:56:20 +0200

    remove heaven & cert, they are unique and named and don't need amenity-icons

--- icons/cccamp15-icons/color-all.sh
+++ icons/cccamp15-icons/color-all.sh
@@ -2,8 +2,5 @@
 
 ./color.sh restrooms   00A6F9
 ./color.sh shower      00A6F9
-./color.sh firstaid    FC00B0
-./color.sh heavencloud 7CFFF6
-./color.sh lan         3F00FF
-./color.sh powerplug   FFE100
+./color.sh powerplug   C90039
 ./color.sh catering    20B200
